f7839d37c5b53f093fed724e3209f46db641e94d
Philip Gonzalez <pgonzalez115@gmail.com>
2018-05-30 11:20:48 -0400

    Add initial routing and dummy controller for '/contacts'

---
+++ server/controllers/contactsController.js
@@ -0,0 +1,24 @@
+var contactsModels = require('../models/padModels.js');
+var url = require('url');
+
+module.exports = {
+  get: (req, res) => {
+    res.send(200, req.body);
+    //link this to the model here
+  },
+
+  post: (req, res) => {
+    res.send(200, req.body);
+    //link this to the model here
+  },
+
+  patch: (req, res) => {
+    res.send(200, req.body);
+    //link this to the model here
+  },
+
+  delete: (req, res) => {
+    res.send(200, req.body);
+    //link this to the model here
+  },
+}
---
+++ server/routes/contactsRouts.js
@@ -0,0 +1,9 @@
+const contactsController = require('../controllers/contactsController.js');
+const contactsRouter = require('express').Router();
+
+contactsRouter.get('/', contactsController.get);
+contactsRouter.post('/', contactsController.post);
+contactsRouter.patch('/', contactsController.patch);
+contactsRouter.delete('/', contactsController.delete);
+
+module.exports = contactsController;
