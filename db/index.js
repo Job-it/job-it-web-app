e2052ab2309134d6dd6ecc6e8a4fcda13a02b21b
Arjun Logeswaran <arjun.logeswaran121@gmail.com>
2018-05-30 12:11:39 -0400

    Db initial setup

--- db/index.js
+++ db/index.js
@@ -1,1 +1,38 @@
 //this is where we connect to the server and export it to home.
+const mongoose = require('mongoose'); 
+mongoose.connect('mongodb://localhost/myapp'); 
+const Schema = mongoose.Schema; 
+
+let usersSchema = new Schema({
+	firstName: String, 
+	lastName: String,
+	githubOAuth: String
+
+}); 
+
+let opportunitiesSchema = new Schema({
+	userFK: String,
+	dateOpened: { type: Date, default: Date.now },
+	dateClosed: Date,
+	orgName: String,
+	rank: String,
+	status: String,
+	type: String,
+	orderInOpportunityList: Number
+}); 
+
+let contactsSchema = new Schema({
+	opportunityFK: String,
+	firstName: String,
+	lastName: String,
+	lastContact: Date,
+	notes: Array
+});
+
+let tasksSchema = new Schema({
+	opportunityFK: String,
+	content: Array,
+	completion: Boolean, 
+	dueDate: Date,
+	status: String
+})
