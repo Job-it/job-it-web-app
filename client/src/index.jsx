2f76b5555fa41a5420bcfb4b809f84be3f5d5c9a
Philip Gonzalez <pgonzalez115@gmail.com>
2018-05-30 17:15:06 -0400

    Implement opportunity form

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,6 +2,4 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/app.jsx'
 
-// TODO: Render the `App` component to the DOM
-
 ReactDOM.render(<App />, document.getElementById('app'));
