bf7419e6ef5f69473e71b7d20e2174175a98868d
nickrzip <rogers.a.nick@gmail.com>
2018-05-31 14:24:08 -0400

    Committing model examples for opportunities

--- server/models/opportunitiesModel.js
+++ server/models/opportunitiesModel.js
@@ -24,21 +24,21 @@ exports.updateOpp = (params, updateObj) => {
   return opportunity.findByIdAndUpdate(params, updateObj)
 }
 
-exports.removeOpp = (dataObj) => {
-  return opportunity.findOneAndUpdate(params, updateObj)
+exports.deleteOpp = (params) => {
+  return opportunity.findOneAndDelete(params)
 }
 
 // EXAMPLE USE OF SAVEOPP
-exports.saveOpp({
-  userFK : '1234',
-  dateOpened : Date.now(),
-  dateClosed : Date.now() + 100,
-  orgName : 'Career Fair',
-  oppName : 'Indeed',
-  rank : '5',
-  status : 'Negotiation',
-  type : ''
-});
+// exports.saveOpp({
+//   userFK : '1234',
+//   dateOpened : Date.now(),
+//   dateClosed : Date.now() + 100,
+//   orgName : 'Career Fair',
+//   oppName : 'Indeed',
+//   rank : '5',
+//   status : 'Negotiation',
+//   type : ''
+// });
 
 // EXAMPLE USE OF GETOPPS
 // exports.getOpps().then((data) => {
@@ -48,4 +48,9 @@ exports.saveOpp({
 // EXAMPLE USE OF UPDATEOPP
 // exports.updateOpp({_id: "5b10367ceb8284e436f46347"}, {orgName: "Starbucks"}).then((data) => {
 //   console.log(data);
+// });
+
+//EXAMPLE OF DELETEOPP
+// exports.deleteOpp({_id: "5b103c216f0fa0e6e59e57ad"}).then((err) => {
+//   console.log(err);
 // });
