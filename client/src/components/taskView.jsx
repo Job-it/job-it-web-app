218e56c41d1e1a249676584dd961651451f722a7
Philip Gonzalez <pgonzalez115@gmail.com>
2018-05-31 15:28:53 -0400

    Implement task form component on react front end

--- client/src/components/taskView.jsx
+++ client/src/components/taskView.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import Modal from 'react-modal';
+import axios from 'axios';
 import TaskColumn from './taskColumn.jsx';
+import TaskForm from './forms/taskForm.jsx';
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)',
+    width: '80%',
+    height: '50%',
+
+  }
+};
 
 class TaskView extends React.Component {
 
   constructor(props) {
     super(props);
+    console.log(props);
     this.state = {
         stages: ['Backlog', 'In Progress', 'Ready for Review', 'Completed'],
         taskForm: false,
@@ -46,8 +64,22 @@ class TaskView extends React.Component {
           }
         ]
     };
+    this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+
     this.getTasks = this.getTasks.bind(this);
     this.addTask = this.addTask.bind(this);
+  }
+
+  openModal() {
+    this.setState({modalIsOpen: true});
+  }
+  afterOpenModal() {
+  //
+  }
+  closeModal() {
+    this.setState({modalIsOpen: false});
   }
 
   addTask() {
@@ -63,9 +95,21 @@ class TaskView extends React.Component {
   render() {
     return (
       <div>
-        <button onClick={() => this.props.switchViews()}>Back to Opportunities List</button><br/>
-        <button onClick={() => this.addTask()}>Add Task</button>
-        { this.state.taskForm ? <TaskForm/> : <div></div> }
+        <div>
+          <button onClick={() => this.props.switchViews()}>Back to Opportunities List</button><br/>
+          <button onClick={this.openModal}>Add New Task</button>
+        </div>
+            <Modal
+              isOpen={this.state.modalIsOpen}
+              onAfterOpen={this.afterOpenModal}
+              onRequestClose={this.closeModal}
+              style={customStyles}
+              contentLabel="New Job.it Opportunity"
+            >
+    
+              <button onClick={this.closeModal}>X</button>
+              <TaskForm currentOpportunity={this.props.currentOpportunity} />
+            </Modal>
         { this.state.stages.map((stage) => 
           <TaskColumn currentOpportunity={this.props.currentOpportunity} stage={stage} tasks={this.state.tasks.filter((task) => {return task.status === stage})}/>)
         }
