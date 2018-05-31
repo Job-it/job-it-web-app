a05495d024ab44c27bf195a40f3f8b45c5dff75d
John Cynn <johncynn@gmail.com>
2018-05-31 12:37:39 -0400

    added onClick for each opportunity card to render taskView

--- client/src/components/opportunityView.jsx
+++ client/src/components/opportunityView.jsx
@@ -1,7 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
 import Modal from 'react-modal';
-
 import OpportunityColumn from './opportunityColumn.jsx';
 import OpportunityForm from './forms/opportunityForm.jsx';
 
@@ -19,77 +17,76 @@ const customStyles = {
   }
 };
 
-Modal.setAppElement('#app')
+Modal.setAppElement('#app');
 
-class opportunityView extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-          stages: ['Exploratory', 'Qualified', 'Outreach', 'Communication', 'Negotiation'],
-          modalIsOpen: false, 
-          opportunities: [
-            {
-              id: 1,
-              User_FK: 1000,
-              Date_Opened: 'Jan. 1, 2018',
-              Date_Closed: false,
-              Opportunity_Name: 'Job Opportunity',
-              Organization_Name: 'Google',
-              Rank: 5, 
-              Stage: 'Exploratory',
-              Type: 'Job Opportunity'
-            },
-            {
-              id: 1,
-              User_FK: 1000,
-              Date_Opened: 'Jan. 1, 2018',
-              Date_Closed: false,
-              Opportunity_Name: 'Networking Event',
-              Organization_Name: 'Google',
-              Rank: 5, 
-              Stage: 'Qualified',
-              Type: 'Job Opportunity'
-            },
-            {
-              id: 1,
-              User_FK: 1000,
-              Date_Opened: 'Jan. 1, 2018',
-              Date_Closed: false,
-              Opportunity_Name: 'Job Opening',
-              Organization_Name: 'Google',
-              Rank: 5, 
-              Stage: 'Negotiation',
-              Type: 'Job Opportunity'
-            },
-            {
-              id: 1,
-              User_FK: 1000,
-              Date_Opened: 'Jan. 1, 2018',
-              Date_Closed: false,
-              Opportunity_Name: 'Job Opening',
-              Organization_Name: 'Google',
-              Rank: 5, 
-              Stage: 'Outreach',
-              Type: 'Job Opportunity'
-            },
-            {
-              id: 1,
-              User_FK: 1000,
-              Date_Opened: 'Jan. 1, 2018',
-              Date_Closed: false,
-              Opportunity_Name: 'Job Opening',
-              Organization_Name: 'Google',
-              Rank: 5, 
-              Stage: 'Communication',
-              Type: 'Job Opportunity'
-            }
-          ]
-      }
-      this.openModal = this.openModal.bind(this);
-      this.afterOpenModal = this.afterOpenModal.bind(this);
-      this.closeModal = this.closeModal.bind(this);
+class OpportunityView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stages: ['Exploratory', 'Qualified', 'Outreach', 'Communication', 'Negotiation'],
+      modalIsOpen: false, 
+      opportunities: [
+        {
+          id: 1,
+          User_FK: 1000,
+          Date_Opened: 'Jan. 1, 2018',
+          Date_Closed: false,
+          Opportunity_Name: 'Job Opportunity',
+          Organization_Name: 'Google',
+          Rank: 5, 
+          Stage: 'Exploratory',
+          Type: 'Job Opportunity'
+        },
+        {
+          id: 2,
+          User_FK: 1000,
+          Date_Opened: 'Jan. 1, 2018',
+          Date_Closed: false,
+          Opportunity_Name: 'Networking Event',
+          Organization_Name: 'Google',
+          Rank: 5, 
+          Stage: 'Qualified',
+          Type: 'Job Opportunity'
+        },
+        {
+          id: 3,
+          User_FK: 1000,
+          Date_Opened: 'Jan. 1, 2018',
+          Date_Closed: false,
+          Opportunity_Name: 'Job Opening',
+          Organization_Name: 'Google',
+          Rank: 5, 
+          Stage: 'Negotiation',
+          Type: 'Job Opportunity'
+        },
+        {
+          id: 4,
+          User_FK: 1000,
+          Date_Opened: 'Jan. 1, 2018',
+          Date_Closed: false,
+          Opportunity_Name: 'Job Opening',
+          Organization_Name: 'Google',
+          Rank: 5, 
+          Stage: 'Outreach',
+          Type: 'Job Opportunity'
+        },
+        {
+          id: 5,
+          User_FK: 1000,
+          Date_Opened: 'Jan. 1, 2018',
+          Date_Closed: false,
+          Opportunity_Name: 'Job Opening',
+          Organization_Name: 'Google',
+          Rank: 5, 
+          Stage: 'Communication',
+          Type: 'Job Opportunity'
+        }
+      ]
     }
+    this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
 
     openModal() {
       this.setState({modalIsOpen: true});
@@ -121,7 +118,7 @@ class opportunityView extends React.Component {
             </Modal>
             <div>
             {this.state.stages.map((stage) => {
-                return <OpportunityColumn stage = {stage} itemsToRender = {this.state.opportunities.filter((opportunity) => {
+                return <OpportunityColumn selectOpportunity={this.props.selectOpportunity} stage={stage} itemsToRender={this.state.opportunities.filter((opportunity) => {
                     return opportunity.Stage === stage;
                 })}/>
             })}
@@ -132,4 +129,4 @@ class opportunityView extends React.Component {
 
 }
 
-export default opportunityView
+export default OpportunityView;
--- db/index.js
+++ db/index.js
@@ -1,5 +1,4 @@
 //this is where we connect to the server and export it to home.
-
 var mongoose = require('mongoose');
 var databasePath = process.env.DATABASE_PATH || 'mongodb://localhost/jobit';
 mongoose.connect(databasePath);
@@ -46,3 +45,8 @@ let tasksSchema = new Schema({
 	dueDate: Date,
 	status: String
 })
+
+module.exports.usersSchema = usersSchema;
+module.exports.opportunitiesSchema = opportunitiesSchema;
+module.exports.contactsSchema = contactsSchema;
+module.exports.tasksSchema = tasksSchema;
