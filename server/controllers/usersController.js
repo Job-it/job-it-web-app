a8ad8424965f31d7e1fc48698fcabf1fd408d918
Arjun Logeswaran <arjun.logeswaran121@gmail.com>
2018-05-31 16:02:19 -0400

    Users route incoming

---
+++ server/controllers/usersController.js
@@ -0,0 +1,17 @@
+var usersModels = require('../models/usersModel.js');
+var url = require('url');
+
+module.exports = {
+	get: (req, res) => {
+
+	},
+	post: (req, res) => {
+		
+	},
+	patch: (req, res) => {
+		
+	},
+	delete: (req, res) => {
+		
+	}
+}
