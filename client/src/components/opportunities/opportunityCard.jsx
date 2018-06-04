cda7da483b8951179c1fd78ecef5701f98a7f838
Philip Gonzalez <pgonzalez115@gmail.com>
2018-06-04 17:13:30 -0400

    Add dropdown functinality for opportunity card actions

--- client/src/components/opportunities/opportunityCard.jsx
+++ client/src/components/opportunities/opportunityCard.jsx
@@ -1,22 +1,55 @@
 import React from 'react';
 import moment from 'moment';
 
-const OpportunityCard = (props) => {
-  return (
-  <div 
-    id='yes-drop'
-    className='opportunity-card draggable-opportunity drag-drop '
-    data-id = {props.opportunity._id}>
-    <button onClick={() => props.selectOpportunity(props.opportunity._id)}> Select Opp </button>
-    <button onClick = {() => {props.deleteOpp(props.opportunity._id)}}> Delete </button>
-    <button onClick = {() => {props.update(props.opportunity._id)}}> Update </button>
-    <button onClick = {() => {props.archiveOpportunity(props.opportunity._id)}}> Archive </button>
-    <div className="opp-info"><h3>{props.opportunity.oppName}</h3></div>
-    <div className="opp-info"><h3>{props.opportunity.orgName}</h3></div>
-    <div className="opp-info">Created {moment(props.opportunity.dateOpened).fromNow()}</div>
-    <div className="opp-info">{props.opportunity.type}</div>
-  </div>
-  )
-}  
+class OpportunityCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showMenu: false,
+    };
+    this.showMenu = this.showMenu.bind(this);
+    this.closeMenu = this.closeMenu.bind(this);
+  }
+
+  showMenu() {
+    this.setState({ showMenu: true });
+    document.addEventListener('click', this.closeMenu);
+  }
+  closeMenu() {
+    this.setState({ showMenu: false });
+    document.removeEventListener('click', this.closeMenu);
+  }
+
+  render() {
+    return (
+      <div 
+        id='yes-drop'
+        className='opportunity-card draggable-opportunity drag-drop '
+        data-id = {this.props.opportunity._id}>
+        <div><button className='opportunity-card-menu-button' onClick={() => this.showMenu()}>V</button></div>
+        <div className='opportunity-card-menu-wrapper'>
+          {
+            this.state.showMenu
+              ? (
+                <div className='opportunity-card-menu'>
+                  <div><button onClick={() => this.props.selectOpportunity(this.props.opportunity._id)}>Explore</button></div>
+                  <div><button onClick = {() => {this.props.update(this.props.opportunity._id)}}> Update </button></div>
+                  <div><button onClick = {() => {this.props.archiveOpportunity(this.props.opportunity._id)}}> Archive </button></div>
+                  <div><button onClick = {() => {this.props.deleteOpp(this.props.opportunity._id)}}> Delete </button></div>
+                </div>
+              )
+              : (
+                null
+              )
+            }
+        </div>
+        <div className="opp-info"><h3>{this.props.opportunity.oppName}</h3></div>
+        <div className="opp-info"><h3>{this.props.opportunity.orgName}</h3></div>
+        <div className="opp-info">Created {moment(this.props.opportunity.dateOpened).fromNow()}</div>
+        <div className="opp-info">{this.props.opportunity.type}</div>
+      </div>
+    )
+  } 
+} 
 
 export default OpportunityCard;
