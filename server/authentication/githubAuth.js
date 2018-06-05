1a0e6cdc0f1a2d394b4d2eda750023e5aa72eed4
nickrzip <rogers.a.nick@gmail.com>
2018-06-05 12:00:19 -0400

    Adding updates to authentication

--- server/authentication/githubAuth.js
+++ server/authentication/githubAuth.js
@@ -5,7 +5,7 @@ var GitHubStrategy = require('passport-github').Strategy;
 passport.use(new GitHubStrategy({
   clientID: '00aea1a71c55ac88e905',
   clientSecret: '207114035db74e96091b3afae0d2a77ab76f24a3',
-  callbackURL: "http://127.0.0.1:3000/auth/github/callback"
+  callbackURL: "127.0.0.1:9000/auth/github/callback"
 }, 
   function(accessToken, refreshToken, profile, cb){
     console.log(profile);
