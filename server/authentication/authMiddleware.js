7a17fe9ccd9f6dc9fff8e57aa3847f197d36edb5
nickrzip <rogers.a.nick@gmail.com>
2018-06-11 11:08:28 -0400

    Committing with commented code and hidden git hub api keys

--- server/authentication/authMiddleware.js
+++ server/authentication/authMiddleware.js
@@ -1,5 +1,7 @@
 var authMiddleware = function (req, res, next) {
-  //append the user object to the header so the client knows that it is authenticated
+  //Auth middleware checks if the user is authenticated before every step
+  //Auth middleware sends back a stringified version of the req.user field to the client
+  //The client checks if req.user is undefined and redirects to the login (this info is also used to set the users name in the nav bar)
   res.append('user', JSON.stringify(req.user));
   if (req.path === '/auth/github' || req.path === '/auth/github/callback') {
     return next();
